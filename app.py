3b7e37ba4de9596cb43c7ec5e2870a42bb7d4423
Alessandro Cartegni <acartegni@gmail.com>
2017-11-13 00:40:25 -0500

    k14

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ import json
 import urllib2
 
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    uResp = urllib2.urlopen(https://api.nasa.gov/planetary/apod?api_key=ElpxYSG5sKBZ4PjXbK6EROAt2nnr5QtKwqqDt7e7)
-    d = json.loads(uResp.read())
-    return render_template('home.html', title = d["title"])
+    #resp = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=ElpxYSG5sKBZ4PjXbK6EROAt2nnr5QtKwqqDt7e7')
+    resp = urllib2.urlopen('http://www.omdbapi.com/?i=tt3896198&apikey=68f529d5')
+    j = resp.read()
+    d = json.loads(j)
+    return render_template('home.html', title = d["Title"], director = d["Director"], actors = d["Actors"], plot = d["Plot"], poster = d["Poster"])
 
 
 
